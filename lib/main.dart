721d113a31f5d0536c61524acbaabfe6383328e8
Eugene <evgeniiorlov204@gmail.com>
2021-10-23 12:09:39 +0300

    Disabled landscape mode for the entire app

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,16 @@ import 'package:flutter/material.dart';
 import 'package:hackathon_project/sign_in.dart';
 import 'package:hackathon_project/sign_up.dart';
 import 'package:hackathon_project/sign_up_extended.dart';
+import 'package:flutter/services.dart';
 
 import 'my_profile.dart';
+
 ///TODO: fix design on big screens
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
   runApp(MaterialApp(
     // Start the app with the "/" named route. In this case, the app starts
     // on the FirstScreen widget.
