9db6471eb81fcaadc39ab62b3a25b3716a249723
vladikTank221 <xxx.vladius.xxx@gmail.com>
2021-10-24 13:24:10 +0300

    implemented user model Class

--- lib/ui/my_profile.dart
+++ lib/ui/my_profile.dart
@@ -1,15 +1,49 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hackathon_project/model/user.dart';
+import 'package:hackathon_project/utils/validator.dart';
+import 'package:image_picker/image_picker.dart';
 
 class MyProfile extends StatefulWidget {
   const MyProfile({Key? key}) : super(key: key);
 
   @override
   State<MyProfile> createState() => _MyProfileState();
-}
+
+
+  }
+
 
 class _MyProfileState extends State<MyProfile> {
   @override
+
+  final GlobalKey<FormState> _formUserNameKey = GlobalKey<FormState>();
+  String dropdownValue = 'Institute One';
+  String imagePath = "";
+  UserModel user = UserModel();
+  bool _loaded = false;
+  var placeholder = const AssetImage("assets/images/ic_user_mockup.png");
+
+  _imgFromGallery() async {
+    XFile? image = await ImagePicker()
+        .pickImage(source: ImageSource.gallery, imageQuality: 50);
+
+    setState(() {
+      if (image == null) {
+        imagePath = "";
+        _loaded = false;
+      } else {
+        imagePath = image.path;
+        _loaded = true;
+        user.imagePath = imagePath;
+      }
+    });
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
@@ -20,23 +54,63 @@ class _MyProfileState extends State<MyProfile> {
         ),
         child: Column(
           children: <Widget>[
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Padding(
-                padding: EdgeInsets.all(24.0),
-                child: Text(
-                  'Settings',
-                  style: GoogleFonts.openSans(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w600,
-                    fontSize: 24.0,
+            Row(
+              children: [
+                //кнопка Settings
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 24, right: 0, top: 4),
+                    child: TextButton(
+                      onPressed: () {
+                        color: Colors.grey;
+                        print('Just pressed');
+                      },
+                      style: TextButton.styleFrom(
+                        backgroundColor: Colors.transparent,
+                      ),
+                      child: Text(
+                        'Settings',
+                        style: GoogleFonts.openSans(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 24.0,
+                        ),
+                      ),
+                    ),
                   ),
                 ),
-              ),
+                //кнопка Log Out
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 325, right: 0, top: 4),
+                    child: TextButton(
+                      onPressed: () {
+                        color: Colors.grey;
+                        print('Just pressed');
+                      },
+                      style: TextButton.styleFrom(
+                        backgroundColor: Colors.transparent,
+                      ),
+                      child: Text(
+                        'Log Out',
+                        style: GoogleFonts.openSans(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 24.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ]
             ),
+            //фотокарточка
             Container(
-              width: 200,
-              height: 200,
+              width: 150,
+              height: 150,
+              padding: EdgeInsets.only(left: 0, right: 0, top: 40),
               decoration:  const BoxDecoration(
                 shape: BoxShape.circle,
                 image:  DecorationImage(
@@ -45,6 +119,42 @@ class _MyProfileState extends State<MyProfile> {
                 ),
               ),
             ),
+            //user_name
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: SizedBox(
+                width: double.infinity,
+                child: DropdownButton<String>(
+                  value: dropdownValue,
+                  icon: const Icon(Icons.account_balance),
+                  iconSize: 24,
+                  elevation: 16,
+                  style: const TextStyle(color: Colors.black45),
+                  underline: Container(
+                    height: 2,
+                    color: Colors.white,
+                  ),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      dropdownValue = newValue ?? "";
+                      user.institute = dropdownValue;
+                    });
+                  },
+                  items: <String>[
+                    'Institute One',
+                    'Institute Two',
+                    'Institute Three',
+                    'Institute Four'
+                  ].map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+              ),
+            ),
+            //нижние кнопки
             Expanded(
               child: Container()
             ),
@@ -80,3 +190,9 @@ class _MyProfileState extends State<MyProfile> {
     );
   }
 }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
